602edbca41961c56835d5eedca90d8b4e685d876
VLC-05L <60168194+VLC-05L@users.noreply.github.com>
2020-04-09 23:13:58 +1000

    Update styles.js
    Testing

--- styles.js
+++ styles.js
@@ -2,6 +2,6 @@
 <script>
    $(document).ready(function() {
      $head = $('#header');
-     $head.prepend('<img src=\"./uon-logo-square.jpg" style=\"float: right;width: 100px;\"/>')
+     $head.prepend('<img src=\".\uon-logo-square.jpg" style=\"float: right;width: 100px;\"/>')
    });
 </script>
