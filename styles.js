4f2914588395c274bc7d8cc4c8a479249d02e396
VLC-05L <60168194+VLC-05L@users.noreply.github.com>
2020-04-09 22:20:14 +1000

    Mass update
    Update all file names. Update references to file names.
    
    Update style files to change the looks.

---
+++ styles.js
@@ -0,0 +1,7 @@
+
+<script>
+   $(document).ready(function() {
+     $head = $('#header');
+     $head.prepend('<img src=\"./uon-logo-square.jpg" style=\"float: right;width: 100px;\"/>')
+   });
+</script>
